fd682ccc6035d21e7410e445812a0f34c16680a2
angelo-francisco <ics20080729@gmail.com>
2025-05-07 04:39:49 +0100

    ignoring compressed files

--- staticfiles/CACHE/js/output.9beb6073d4d6.js
+++
@@ -1,7 +0,0 @@
-const menuToggle=document.querySelector('.menu-toggle')
-const navLinks=document.querySelector('.nav-links')
-menuToggle.addEventListener('click',()=>{navLinks.classList.toggle('active')
-menuToggle.classList.toggle('active')})
-const navItems=document.querySelectorAll('.nav-links a')
-navItems.forEach((item)=>{item.addEventListener('click',()=>{navLinks.classList.remove('active')
-menuToggle.classList.remove('active')})});
