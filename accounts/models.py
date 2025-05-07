b71e680ee318112bed2356f838c86f90526b6090
angelo-francisco <ics20080729@gmail.com>
2025-05-07 19:30:25 +0100

    setting the subscriber funcs

--- accounts/models.py
+++ accounts/models.py
@@ -3,10 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     @property
-    def actual_subscription_plan(self):
+    def subscription(self):
         try:
             return (
                 self.subscriptions.filter(is_active=True).select_related("plan").first()
             )
         except Exception:
             return None
+    
