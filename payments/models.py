bc86d98338a1731ad48c94507c102b1b88c98e92
angelo-francisco <ics20080729@gmail.com>
2025-05-07 16:01:01 +0100

    new models: plan, subscription and resource usage

--- payments/models.py
+++ payments/models.py
@@ -1,10 +1,32 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
-class Plan(models.Model): ...
+class Plan(models.Model):
+    name = models.CharField(max_length=50, null=True, blank=True)
+    limits = models.JSONField(default=dict)
+
+    def __str__(self):
+        return self.name
 
 
-class ResouceUsage(models.Model): ...
+class Subscription(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="subscriptions"
+    )
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    raw_data = models.JSONField(default=dict, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.user} - {self.plan.name}"
 
 
-class Subscription(models.Model): ...
+class ResouceUsage(models.Model):
+    subscription = models.ForeignKey(Subscription, on_delete=models.CASCADE, )
+    resource = models.CharField(max_length=50, null=True, blank=True)
+    amount_used = models.PositiveIntegerField(default=0)
