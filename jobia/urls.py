f71624ed34db2f93ef3971f35a74e023a80781db
angelo-francisco <ics20080729@gmail.com>
2025-05-09 07:29:34 +0100

    adicionando webhooks

--- jobia/urls.py
+++ jobia/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import home_page, dashboard, new_curriculum
+from .views import home_page, dashboard, new_curriculum, typeform_webhook, generate_curriculum
 
 
 urlpatterns = [
     path("", home_page, name="home_page"),
     path("dashboard/", dashboard, name="dashboard"),
-    path("curriculum/new/", new_curriculum, name="new_curriculum")
+    path("curriculum/new/", new_curriculum, name="new_curriculum"),
+    path("curriculum/generate/", generate_curriculum, name="generate_curriculum"),
+    path("curriculum/typeform-webhook/", typeform_webhook, name="typeform_webhook")
 ]
--- jobia/views.py
+++ jobia/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+import json
+
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_exempt
 
 from payments.decorators import user_has_feature_access
+
 
 def home_page(request):
     print(request.user.is_authenticated)
@@ -12,11 +16,25 @@ def home_page(request):
 @login_required
 @never_cache
 def dashboard(request):
-    print(request.user.subscription.get_remaining_usage('max_curriculos'))
+    print(request.user.subscription.get_remaining_usage("max_curriculos"))
     return render(request, "jobia/dashboard.html")
 
 
 @login_required
-@user_has_feature_access('max_curriculos')
+@user_has_feature_access("max_curriculos")
 def new_curriculum(request):
-    return render(request, 'jobia/new_curriculum.html')
+    return render(request, "jobia/new_curriculum.html")
+
+
+@login_required
+@csrf_exempt
+def typeform_webhook(request):
+    if request.method == "POST":
+        payload = json.loads(request.body)
+        request.session["typeform-payload"] = payload
+        return redirect("")
+    return redirect("dashboard")
+
+
+@login_required
+def generate_curriculum(request): ...
