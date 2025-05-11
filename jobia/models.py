ba4fbc03fa980b0b7b0a71f1465298270a3756d4
angelo-francisco <ics20080729@gmail.com>
2025-05-11 19:06:52 +0100

    nova dependencia para gerar uuids curtos

--- jobia/models.py
+++ jobia/models.py
@@ -5,8 +5,25 @@ User = settings.AUTH_USER_MODEL
 
 
 class Curriculum(models.Model):
+    """
+    Model Curriculum
+
+    Contains all the data related to the generated curriculums
+    """
+
+    class StatusChoices(models.TextChoices):
+        COMPLETE = 'C', 'Complete'
+        INCOMPLETE = 'I', 'Incomplete'
+
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    title = models.CharField(max_length=80, null=True, blank=True)
-    form_data = models.JSONField(default=dict, null=True, blank=True)
-    curriculum = models.FileField(upload_to="curriculums", null=True, blank=True)
-    created_at = models.DateTimeField(auto_now=True)
+    title = models.CharField('Title', max_length=80, null=True, blank=True)
+    status = models.CharField('Status', max_length=10, null=True, blank=True, choices=StatusChoices, default=StatusChoices.INCOMPLETE)
+    form_data = models.JSONField('Form Raw Data', default=dict, null=True, blank=True)
+    curriculum = models.FileField('Curriculum', upload_to="curriculums", null=True, blank=True)
+    created_at = models.DateTimeField('Creatio Date', auto_now=True)
+    slug = models.SlugField("Slug", null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            
+        return super().save(*args, **kwargs)
