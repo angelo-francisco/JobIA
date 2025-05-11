e612a21130fbc5065a9b3e7a61f00213cf40932b
angelo-francisco <ics20080729@gmail.com>
2025-05-11 21:21:46 +0100

    refactoring

--- jobia/urls.py
+++ jobia/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
-from .views import home_page, dashboard, new_curriculum, generate_curriculum, get_form_data
+from .views import (
+    home_page,
+    dashboard,
+    new_curriculum,
+    generate_curriculum,
+    get_form_data,
+    delete_curriculum
+)
 
 
 urlpatterns = [
     path("", home_page, name="home_page"),
     path("dashboard/", dashboard, name="dashboard"),
     path("curriculum/new/", new_curriculum, name="new_curriculum"),
-    path("curriculum/generate/<slug>/", generate_curriculum, name="generate_curriculum"),
-    path("curriculum/get-form-data/", get_form_data, name="get_form_data")
+    path(
+        "curriculum/generate/<slug>/", generate_curriculum, name="generate_curriculum"
+    ),
+    path("curriculum/get-form-data/", get_form_data, name="get_form_data"),
+    path("curriculum/del/<slug>/", delete_curriculum, name="delete_curriculum")
 ]
